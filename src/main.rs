8d120e605e2b416ee656fb0cdf284206408498b1
Michael Zill <michael.zill@gmail.com>
2022-11-16 13:00:13 +0100

    embassy version with timer issue

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,29 @@
 use core::str;
 use embassy_sync::blocking_mutex::Mutex;
+
+use embassy_time::{Duration, Timer};
+//use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
-use esp_idf_hal::interrupt;
+
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::task::embassy_sync::EspRawMutex;
-use esp_idf_hal::task::CriticalSection;
+use esp_idf_hal::task::executor::EspExecutor;
+
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use esp_idf_sys::{self as sys, esp, EspError};
 use log::info;
-use std::borrow::BorrowMut;
+
 use std::cell::RefCell;
 use std::sync::Arc;
-type LedPin = esp_idf_hal::gpio::Gpio18;
-use embassy_sync::channel::Channel;
-use embassy_time::{Duration, Timer};
-use esp_idf_hal::task::executor::EspExecutor;
 
-static NEOPIXEL_PIN: static_cell::StaticCell<Mutex<EspRawMutex, RefCell<LedPin>>> =
+type LedPin = esp_idf_hal::gpio::PinDriver<'static, Gpio7, Output>;
+
+static LED_PIN: static_cell::StaticCell<Arc<Mutex<EspRawMutex, RefCell<LedPin>>>> =
     static_cell::StaticCell::new();
-
-static GLOBAL_TEXT: static_cell::StaticCell<Arc<Mutex<EspRawMutex, RefCell<String>>>> =
-    static_cell::StaticCell::new();
-
-static CHANNEL: Channel<EspRawMutex, LedState, 1> = Channel::new();
-
-enum LedState {
-    On,
-    Off,
-}
 
 sys::esp_app_desc!();
 
 fn main() -> Result<(), EspError> {
-    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
-    // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
-
-    //NEOPIXEL_PIN.lock(|NEOPIXEL_PIN| NEOPIXEL_PIN.borrow_mut().
-    //let x = NEOPIXEL_PIN.get();
-
     init()?;
     run()
 }
@@ -49,30 +35,6 @@ fn init() -> Result<(), EspError> {
     esp_idf_svc::timer::embassy_time::queue::link();
 
     esp_idf_svc::log::EspLogger::initialize_default();
-
-    info!("Hello, world!");
-
-    let peripherals = Peripherals::take().unwrap();
-    NEOPIXEL_PIN.init(Mutex::new(RefCell::new(peripherals.pins.gpio18)));
-
-    let gt_handle = GLOBAL_TEXT.init(Arc::new(Mutex::new(RefCell::new(String::from(
-        "Global Text",
-    )))));
-
-    let gt = gt_handle.clone();
-    gt.lock(|gt| info!("{}", gt.borrow_mut()));
-
-    gt.lock(|gt| gt.swap(&RefCell::new(String::from("hhh"))));
-
-    let gt2 = gt_handle.clone();
-    gt2.lock(|gt2| info!("{}", gt2.borrow_mut()));
-
-    let test_pin = Mutex::<EspRawMutex, _>::new(RefCell::new(String::from("Local Text")));
-
-    test_pin.lock(|test_pin| info!("{}", test_pin.borrow_mut()));
-
-    test_pin.lock(|test_pin| test_pin.borrow_mut().clear());
-    test_pin.lock(|test_pin| info!("{}", test_pin.borrow_mut()));
 
     unsafe {
         #[allow(clippy::needless_update)]
@@ -88,18 +50,55 @@ fn init() -> Result<(), EspError> {
 }
 
 fn run() -> Result<(), EspError> {
-    let executor = EspExecutor::<16, _>::new();
+    info!("Hello, world!");
 
-    let tasks = [executor
+    let peripherals = Peripherals::take().unwrap();
+    let led_pin_handle = LED_PIN.init(Arc::new(Mutex::new(RefCell::new(
+        PinDriver::output(peripherals.pins.gpio7).unwrap(),
+    ))));
+    let led_pin = led_pin_handle.clone();
+    led_pin.lock(|led_pin| {
+        let mut pin = led_pin.borrow_mut();
+        pin.set_low().unwrap();
+    });
+
+    let executor = EspExecutor::<16, _>::new();
+    let led_pin1 = led_pin_handle.clone();
+
+    let task1 = executor
         .spawn_local(async move {
-            log::info!("starting");
             loop {
-                Timer::after(Duration::from_secs(5)).await;
-                log::info!("woke up");
+                log::info!("task1");
+                Timer::after(Duration::from_secs_floor(2)).await;
+                //Timer::after(Duration::from_secs(2)).await;
+                //embassy_time::Timer::after(embassy_time::Duration::from_millis(500)).await;
+                //FreeRtos::delay_ms(500);
+                led_pin1.lock(|led_pin1| {
+                    let mut pin = led_pin1.borrow_mut();
+                    pin.set_high().unwrap();
+                });
             }
         })
-        .unwrap()];
+        .unwrap();
 
-    executor.run_tasks(|| true, tasks);
+    let led_pin2 = led_pin_handle.clone();
+    let task2 = executor
+        .spawn_local(async move {
+            loop {
+                log::info!("task2");
+                Timer::after(Duration::from_secs_floor(4)).await;
+                //Timer::after(Duration::from_secs(4)).await;
+                //embassy_time::Timer::after(embassy_time::Duration::from_millis(1000)).await;
+                //FreeRtos::delay_ms(1000);
+                led_pin2.lock(|led_pin2| {
+                    let mut pin = led_pin2.borrow_mut();
+                    pin.set_low().unwrap();
+                });
+            }
+        })
+        .unwrap();
+
+    executor.run_tasks(|| true, [task1, task2]);
+
     Ok(())
 }
